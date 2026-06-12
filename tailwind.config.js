d58cd5ea3a87a1f59cfab36a244ec1c42a7550cd
Remy Vanherweghem <remy.vanherweghem@gmail.com>
2026-06-11 20:04:34 -0400

    Compile Tailwind with Vite.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import tailwindcss from "@tailwindcss/vite";
+
 export default {
   content: [
     "./index.html",
@@ -7,6 +8,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+        tailwindcss(),
+  ],
 }
 
